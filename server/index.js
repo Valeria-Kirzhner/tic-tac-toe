9f418afdc64c25179975fa24da6d6054ccd6a782
Valeria Kirzhner <valeria.kirzhner@gmail.com>
2022-11-02 12:18:01 +0200

    corse issue and client cells bug fixed.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-const io = require("socket.io");
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
 const PORT = 8000;
 
 io.on("connection", (socket) => {
